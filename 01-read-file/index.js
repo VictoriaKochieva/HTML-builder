88d19bde986e2875b751c7b098281d0a3d059bc4
VictoriaKochieva <kochievavictoria@gmail.com>
2021-10-29 16:34:48 +0300

    fix: fix 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const fs = require('fs');
+const { stdout } = require('process');
 const filePath = path.join(__dirname, 'text.txt');
 
 const stream = fs.createReadStream(filePath, 'utf-8');
 stream.on('data', partData => {
-  console.log(partData.trim());
+  stdout.write(partData.trim());
 });
