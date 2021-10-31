71cc6db717e8897ebf2e4ebacc29d563d3da8d69
VictoriaKochieva <kochievavictoria@gmail.com>
2021-11-01 00:58:40 +0300

    feat: add solution of the task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+fs.access(path.join(__dirname, 'files-copy'), err => {
+  if(err) {
+    fs.mkdir(path.join(__dirname, 'files-copy'),{recursive: true}, err=>{
+      if(err) throw err;
+    });
+  }
+});
 
+fs.readdir(path.join(__dirname, 'files'), (err,data)=>{
+  if(err) throw err;
+
+  data.forEach(file => {
+    fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), err=>{
+      if(err) throw err;
+    });
+  });
+
+  stdout.write('Copied!\n');
+});
