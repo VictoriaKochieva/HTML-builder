d4a5dd661b1eb9c4d5a10bdd44dabc1dbc2a1fbf
VictoriaKochieva <kochievavictoria@gmail.com>
2021-11-04 12:04:50 +0300

    fix: fix deleting of files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -10,12 +10,22 @@ fs.access(path.join(__dirname, 'files-copy'), err => {
 });
 
 fs.readdir(path.join(__dirname, 'files'), (err,data)=>{
-  if(err) throw err;
+  if(err) throw err;  
 
   data.forEach(file => {
     fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), err=>{
       if(err) throw err;
     });
+  });
+
+  fs.readdir(path.join(__dirname, 'files-copy'), (err, data2) => {
+    for(let i = 0, j = 0; i < data.length, j < data2.length; i++, j++) {
+      if(data[i] !== data2[j]) {
+        fs.unlink( path.join(__dirname, 'files-copy',data2[j]), err=> {
+          if (err) throw err;
+        });
+      }
+    }
   });
 
   stdout.write('Copied!\n');
